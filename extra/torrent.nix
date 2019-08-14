cfceb285ac6e61e3706dc0815aeba116593809de
Timothy DeHerrera <tim.deh@pm.me>
2019-08-13 23:18:28 -0600

    create qbittorrent module and use it as default torrent application

--- extra/torrent.nix
+++ extra/torrent.nix
@@ -1,20 +1,9 @@
 { pkgs, ... }:
 # always use my vpn configuration with torrent service
-{ imports = [ ./vpn.nix ];
+{ imports = [ ./vpn.nix ./modules/qbittorrent.nix ];
 
-  services.transmission =
-  { enable   = true;
-    settings =
-    { download-dir = "/srv/torrents/";
-      incomplete-dir-enabled = false;
-      rpc-whitelist = "127.0.0.1,192.168.8.*";
-      rpc-whitelist-enabled = true;
-      peer-port-random-on-start = true;
-      port-forwarding-enabled = false;
-      # severely hinders speed for some reason
-      utp-enabled = false;
-    };
-  };
 
-  users.users.nrd.extraGroups = [ "transmission" ];
+  services.qbittorrent.enable = true;
+
+  users.users.nrd.extraGroups = [ "qbittorrent" ];
 }
