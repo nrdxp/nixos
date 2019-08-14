b69a9788b071912420d6a399404a7cb735054386
Timothy DeHerrera <tim.deh@pm.me>
2019-08-14 16:06:26 -0600

    move vpn configuration out of vpn.nix and into the usr configuration store

--- extra/torrent.nix
+++ extra/torrent.nix
@@ -5,5 +5,8 @@
 
   services.qbittorrent.enable = true;
 
+  systemd.services.qbittorrent.bindsTo = ["openvpn-client" ];
+  systemd.services.qbittorrent.after = ["openvpn-client" ];
+
   users.users.nrd.extraGroups = [ "qbittorrent" ];
 }
