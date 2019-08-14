37462e65ee38abbb09d467be39608a6e7af74355
Timothy DeHerrera <tim.deh@pm.me>
2019-08-14 16:31:31 -0600

    has no effect removing

--- extra/torrent.nix
+++ extra/torrent.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 # always use my vpn configuration with torrent service
 { imports = [ ./vpn.nix ./modules/qbittorrent.nix ];
-
-
   services.qbittorrent.enable = true;
-
-  systemd.services.qbittorrent.bindsTo = ["openvpn-client" ];
-  systemd.services.qbittorrent.after = ["openvpn-client" ];
-
   users.users.nrd.extraGroups = [ "qbittorrent" ];
 }
