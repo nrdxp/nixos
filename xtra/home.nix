1ac58c731adfcad82f9fb59103befa8cdfbf4156
Timothy DeHerrera <tim.deh@pm.me>
2019-08-17 15:45:21 -0600

    break out xorg home-manager configuration for consistency

--- xtra/home.nix
+++ xtra/home.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, usr, ... }:
+{ lib, usr, ... }:
 {
   imports =
   [ "${builtins.fetchGit https://github.com/rycee/home-manager}/nixos"
@@ -12,7 +12,7 @@
       file = with usr;
       { ".zshrc".text                  = "";
 
-        ".config/git/config".text    = usr.home.gitconfig;
+        ".config/git/config".text    = home.gitconfig;
 
         ".ssh/config".text           = home.ssh.config;
         ".ssh/github".text           = home.ssh.github;
@@ -21,40 +21,7 @@
         ".ssh/gitlab.pub".text       = home.ssh."gitlab.pub";
 
         ".gnupg/gpg-agent.conf".text = home.gnupg."gpg-agent.conf";
-      } // lib.attrsets.optionalAttrs config.services.xserver.enable
-      { ".background-image".source     =
-          "${pkgs.adapta-backgrounds}/share/backgrounds/adapta/tealized.jpg";
-
-        ".local/share/qutebrowser/userscripts".source =
-        ../usr/qutebrowser/scripts;
-
-        ".xinitrc" =
-        { text = "exec xmonad";
-        };
-
-        ".config/qutebrowser/config.py" =
-        {
-          text = "config.source('/etc/qutebrowser/config.py')";
-
-          # install spellchecker
-          onChange =
-          ''
-          ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install en-US
-          '';
-        };
-
-        "shots/dummy" =
-        { onChange =
-          ''
-          rm $HOME/shots/dummy
-          '';
-
-          text = "";
-        };
-
-        ".xmonad/xmonad.hs".text = usr.xmonad."xmonad.hs";
       };
     };
   };
-
 }
---
+++ xtra/xorg/home-manager.nix
@@ -0,0 +1,36 @@
+{ pkgs, usr, ... }:
+{
+  users.nrd.home.file =
+  { ".background-image".source     =
+      "${pkgs.adapta-backgrounds}/share/backgrounds/adapta/tealized.jpg";
+
+    ".local/share/qutebrowser/userscripts".source =
+    ./lib/qutebrowser/scripts;
+
+    ".xinitrc" =
+    { text = "exec xmonad";
+    };
+
+    ".config/qutebrowser/config.py" =
+    {
+      text = "config.source('/etc/qutebrowser/config.py')";
+
+      # install spellchecker
+      onChange =
+      ''
+      ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install en-US
+      '';
+    };
+
+    "shots/dummy" =
+    { onChange =
+      ''
+      rm $HOME/shots/dummy
+      '';
+
+      text = "";
+    };
+
+    ".xmonad/xmonad.hs".text = usr.xmonad."xmonad.hs";
+  };
+}
