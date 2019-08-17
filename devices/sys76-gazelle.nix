c05805b46a88972dbac2139cf7dcb63201c15d12
Timothy DeHerrera <tim.deh@pm.me>
2019-08-17 14:59:21 -0600

    fix polkit asking for permission for intel_backlight

--- devices/sys76-gazelle.nix
+++ devices/sys76-gazelle.nix
@@ -20,6 +20,14 @@
 
   services.xserver.videoDrivers = [ "intel" "modesetting" ];
 
+  security.polkit.extraConfig =
+  ''
+  polkit.addRule(function(a, s) {
+    if (a.id = 'xf86-video-intel-backlight-helper' && s.isInGroup('users'))
+      return polkit.Result.YES;
+  });
+  '';
+
   # enable tearfree
   services.xserver.extraConfig =
   ''
