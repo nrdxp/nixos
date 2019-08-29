b9cb13ec377cb679e846471fc2b1bcd6b615f4d4
Timothy DeHerrera <tim.deh@pm.me>
2019-08-28 18:55:13 -0600

    some additional changes to kakoune configuration
    * use skim for kak-fzf for faster parsing
    * add alias 'k' for kak
    * add 'kakboard' and 'move-line' via direct link (not in nixpkgs yet)
    * remove ad-hoc clipboard implementation
    * change buffer next/preview shortcuts to avoid conflict with search

--- xtra/kak.nix
+++ xtra/kak.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = with pkgs; with pkgs.elmPackages;
   let kak = kakoune.override {
     configure.plugins = with kakounePlugins;
-    [ kak-fzf
+    [ (kak-fzf.override { fzf = pkgs.skim; })
       kak-powerline
       kak-auto-pairs
       kak-vertical-selection
@@ -13,10 +13,26 @@
   in
   [ kak kak-lsp elm elm-analyse elm-format elm-test ];
 
-  home-manager.users.nrd.home.file =
+  programs.zsh.shellAliases =
+  { k = "kak";
+  };
+  home-manager.users.nrd.home.file = with builtins;
   { ".config/kak/kakrc".source = ./kak/kakrc;
     ".config/kak-lsp/kak-lsp.toml".source = ./kak/kak-lsp.toml;
     ".config/kak/autoload/plugins".source = ./kak/plugins;
     ".config/kak/autoload/default".source = "${pkgs.kakoune-unwrapped}/share/kak/rc";
+    ".config/kak/autoload/move-line.kak".text =
+      readFile ( fetchurl
+        https://raw.githubusercontent.com/alexherbo2/move-line.kak/master/rc/move-line.kak
+      ) + ''
+      map global normal "'" ': move-line-below %val{count}<ret>'
+      map global normal "<a-'>" ': move-line-above %val{count}<ret>'
+      '';
+    ".config/kak/autoload/kakboard.kak".text =
+      readFile ( fetchurl
+        https://raw.githubusercontent.com/lePerdu/kakboard/master/kakboard.kak
+      ) + ''
+      hook global WinCreate .* %{ kakboard-enable }
+      '';
   };
 }
