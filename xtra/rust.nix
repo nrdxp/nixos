7c68c99835ddecc6a09078b22cdb015f4c68c7ec
Timothy DeHerrera <tim.deh@pm.me>
2019-09-11 17:09:59 -0600

    rust: use mozilla overlay to make more flexible
    * add wasm targets

--- xtra/rust.nix
+++ xtra/rust.nix
@@ -1,17 +1,33 @@
 { pkgs, ... }:
 let
-  inherit (pkgs.rustPlatform) rustcSrc;
+  inherit (pkgs) clang;
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  inherit (pkgs.latest.rustChannels.stable) cargo rust rust-src;
 in
 {
   imports = [ ./home.nix ];
   environment = {
-    systemPackages = with pkgs;
-    [ rustc rustfmt cargo rls rustracer ];
-
-    sessionVariables = {
-      RUST_SRC_PATH = "${rustcSrc}";
-    };
+    systemPackages =
+      [ (rust.override {
+          extensions = [
+            "rust-src"
+            "rls-preview"
+            "rust-analysis"
+            "rustfmt-preview"
+            "clippy-preview"
+          ];
+          targets = [
+            "wasm32-unknown-unknown"
+            "wasm32-unknown-emscripten"
+            "asmjs-unknown-emscripten"
+          ];
+        })
+        cargo
+        clang
+      ];
   };
 
   home-manager.users.nrd.home.file.".cargo/credentials".source = ./rust/cargo/credentials;
+
+  nixpkgs.overlays = [ moz_overlay ];
 }
