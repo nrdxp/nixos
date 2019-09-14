44516dc869d14160c1fefc6b62d104810cc8ccd4
Timothy DeHerrera <tim.deh@pm.me>
2019-09-14 15:52:16 -0600

    swayidle: separate sleep command
    run two instances of swayidle so that session will still get locked
    before sleep in fullscreen.

--- xtra/sway/swayidle.sh
+++ xtra/sway/swayidle.sh
@@ -7,8 +7,5 @@ trap cleanup EXIT
 
 (echo $$ > /run/user/$UID/swayidle.pid; exec swayidle -w \
   timeout 300 'swaylock -f -c 000000' \
-  timeout 600 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on"' \
-  before-sleep 'swaylock -f -c 000000')
-
-cleanup
+  timeout 300 'swaymsg "output * dpms off"' \
+  resume 'swaymsg "output * dpms on"')
