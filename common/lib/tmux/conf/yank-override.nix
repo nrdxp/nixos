2a356143ee34710780e8b3b10438b77a2f6a33a0
Timothy DeHerrera <tim.deh@pm.me>
2019-09-13 19:05:17 -0600

    tmux: yank override no longer needed
    tmux-yank plugin works properly again and allows both wayland and
    xsession to have a properly functioning clipboard

--- common/lib/tmux/conf/yank-override.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }:
-''
-  # put yank command here to override tmux yank command
-  bind -T copy-mode-vi 'y' send-keys -X copy-pipe '${pkgs.xsel}/bin/xsel -ib' \; send -X clear-selection
-''
--- common/programs/tmux/extraTmuxConf.nix
+++ common/programs/tmux/extraTmuxConf.nix
@@ -1,11 +1,10 @@
 { usr, ... }:
 let
   inherit (builtins) concatStringsSep attrValues;
-  inherit (usr.tmux) conf pluginConf plugins tmuxline;
+  inherit (usr.tmux) pluginConf plugins tmuxline;
 
   tmuxConf = let
-    tmux = concatStringsSep "\n\n"
-      (attrValues conf);
+    tmux = usr.tmux."tmux.conf";
 
     plugin = pluginConf plugins;
   in
