153a33659a922d1938df30ef1ddd10b436a1ecee
Timothy DeHerrera <tim.deh@pm.me>
2019-09-15 21:28:08 -0600

    retroarch: update & add wayland support

--- common/lib/overlays/retroarch.nix
+++ common/lib/overlays/retroarch.nix
@@ -3,12 +3,12 @@ self: super: {
   retroarchBare = super.retroarchBare.overrideAttrs (
     oldAttrs: rec {
       name = "retroarch-bare-${version}";
-      version = "1.7.7";
+      version = "1.7.8.3";
 
       src = self.fetchFromGitHub {
         owner = "libretro";
         repo = "RetroArch";
-        sha256 = "026720z0vpiwr4da7l2x2yinns09fmg6yxsib203xwnixj399azi";
+        sha256 = "0lsfiljy6cin3xsxyh80d6y9a77h7h82fcm6k148cd8mndvyxcdn";
         rev = "v${version}";
       };
 
@@ -16,9 +16,9 @@ self: super: {
       NIX_CXXFLAGS_COMPILE = "-march=native -flto";
       # add mesa_noglu to build inputs for kms support
       buildInputs = let
-        inherit (super) retroarchBare mesa_noglu;
+        inherit (super) retroarchBare mesa_noglu wayland;
       in
-        retroarchBare.buildInputs ++ [ mesa_noglu ];
+        retroarchBare.buildInputs ++ [ mesa_noglu wayland ];
     }
   );
 }
