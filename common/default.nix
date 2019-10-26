nrdxp/nixos

commit 31b395a250d0c6b0ff9effdda42c4222cfb035e0

add common directories to the top-level
Avoid the brittleness of having to repeatedly type common directories by
adding a top level attribute that can be passed around the
configuration.

configuration.nix
@@ -1,8 +1,9 @@
-args@{ config, ... }:
+args@{ config
+, usr ? config.lib
+, dirs ? import ./lib/directories.nix
+, ...
+}:
 {
-  # shortcut to locally defined lib to be easily passed around
-  _module.args.usr = config.lib;
-
   imports = [
     # bare minimum
     ./common.nix
@@ -18,7 +19,7 @@ args@{ config, ... }:
       )
     )
 
-    # add ad hoc configuration from 'xtra'
+    # add ad hoc configurations from 'xtra'
     ./xtra/torrent.nix
     ./xtra/laptop.nix
     ./xtra/games.nix

common/lib/overlays/black_chocobo.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ dirs, ... }:
 self: super: {
-  black_chocobo = super.libsForQt5.callPackage ./pkgs/black_chocobo.nix {};
+  black_chocobo = super.libsForQt5.callPackage "${dirs.pkgs}/black_chocobo.nix" {};
 }

common/lib/overlays/default-cursor.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ dirs, ... }:
 self: super: {
-  default-cursor = super.callPackage ./pkgs/cursor.nix {};
+  default-cursor = super.callPackage "${dirs.pkgs}/cursor.nix" {};
 }

common/lib/overlays/dzvol.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ dirs, ... }:
 self: super: {
-  dzvol = super.callPackage ./pkgs/dzvol.nix {};
+  dzvol = super.callPackage "${dirs.pkgs}/dzvol.nix" {};
 }

common/lib/overlays/htop.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ dirs, ... }:
 self: super:
 {
   htop = super.htop.overrideAttrs (
     oldAttrs: {
-      patches = ./pkgs/patches/htop-vim-keybindings.patch;
+      patches = "${dirs.pkgs}/patches/htop-vim-keybindings.patch";
     }
   );
 }

common/lib/overlays/passdmenu.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ dirs, ... }:
 self: super: {
-  passdmenu = super.callPackage ./pkgs/passdmenu.nix {};
+  passdmenu = super.callPackage "${dirs.pkgs}/passdmenu.nix" {};
 }

common/lib/overlays/st.nix
@@ -1,7 +1,7 @@
-{ ... }:
+{ dirs, ... }:
 self: super: {
   st = super.st.override
     {
-      patches = ./pkgs/patches/st-config.patch;
+      patches = "${dirs.pkgs}/patches/st-config.patch";
     };
 }

common/lib/overlays/widevine.nix
@@ -1,5 +1,5 @@
-{ ... }:
+{ dirs, ... }:
 self: super:
 {
-  widevine = super.callPackage ../../../pkgs/widevine.nix {};
+  widevine = super.callPackage "${dirs.pkgs}/widevine.nix" {};
 }

commit 1802787f68a69bd597bb128f9642b1d89aaf20bc

fix deployment issue caused by 31b395a250d

common/default.nix
@@ -1,4 +1,4 @@
-args@{ pkgs, usr, ... }:
+args@{ pkgs, usr, dirs, ... }:
 let
   recImport = import ../lib/recImport.nix args;
 in

configuration.nix
@@ -1,9 +1,8 @@
-args@{ config
-, usr ? config.lib
-, dirs ? import ./lib/directories.nix
-, ...
-}:
+args@{ config, ... }:
 {
+  _module.args.usr = config.lib;
+  _module.args.dirs = import ./lib/directories.nix;
+
   imports = [
     # bare minimum
     ./common.nix